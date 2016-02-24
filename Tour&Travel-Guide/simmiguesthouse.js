1c2df017ae0c09f0eae1e637a8fa50d19bd792c3
narayanbhagat <bhagat.nr22@gmail.com>
2016-02-24 18:32:35 +0530

    GuestHouseUpdate

---
+++ Tour&Travel-Guide/simmiguesthouse.js
@@ -0,0 +1,18 @@
+var amountScrolled = 300;
+
+$(document).ready(function(){
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+
+$('a.back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
+});
+})
